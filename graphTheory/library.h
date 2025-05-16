77594b307ea35b2e1d7eb00a9de568a15d7562c0
Pham Do Tien Dat <144351345+DatPhamDoTien@users.noreply.github.com>
2025-05-16 15:50:10 +0700

    Update library.h

--- graphTheory/library.h
+++ graphTheory/library.h
@@ -1,7 +1,6 @@
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <fstream>
-#include <cstdio>
+
 using namespace std;
 
 const int MAXV = 20;
@@ -28,10 +27,18 @@ struct adjacencyMatrix {
 void printVertex(VERTEX v);
 void printArrayVertex(arrayVertex arr);
 void printArrayEdge(arrayEdge arr);
-int readFile(const char* fileName, adjacencyMatrix& mt);
-int writeFile(const char* fileName, adjacencyMatrix& mt);
+int readFile(const char *fileName, adjacencyMatrix& mt);
+int writeFile(const char *fileName, adjacencyMatrix& mt);
 void printAdjacencyMatrix(adjacencyMatrix mt);
 int calDegreeInDirected(adjacencyMatrix mt, int vertex);
 arrayEdge direcredGraphEdge(adjacencyMatrix mt);
-void initMatrix(adjacencyMatrix& mt, int numOfVertex);
-adjacencyMatrix matrixDecoding(arrayEdge& edges, int numOfVertex);
+//bài tập tổng hợp.
+//Bài 1
+adjacencyMatrix initMatrix(adjacencyMatrix& mt, int numOfVertex);
+adjacencyMatrix matrixDecoding(arrayEdge edges, int numOfVertex);
+//bài 2
+adjacencyMatrix matrixDecodingDeriectional(arrayEdge edges, int numOfVerTex);
+//bài 3
+VERTEX findLightestVertex(adjacencyMatrix mt, VERTEX verU);
+//bài 4
+adjacencyMatrix redirectMatrix(adjacencyMatrix& mt);
