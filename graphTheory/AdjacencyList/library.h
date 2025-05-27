9928a69866d0ff3c92ec4d97d02c1cafcd4df4ff
Pham Do Tien Dat <144351345+DatPhamDoTien@users.noreply.github.com>
2025-05-27 22:43:54 +0700

    Update library.h

--- graphTheory/AdjacencyList/library.h
+++ graphTheory/AdjacencyList/library.h
@@ -1,21 +1,22 @@
 #define _CRT_SECURE_NO_WARINGS
+#include <chrono>
 #include <iostream>
+#include <mutex>
 #include <thread>
 #include <vector>
-#include<chrono>
-#include<mutex>
 using namespace std;
 
 const int MAXV = 15;          // so dinh toi da
 const int MAXE = MAXV * MAXV; // so canh toi da
+
 struct NODE {
   int adjacentVertex;
   int weight = 0;
-  NODE* next;
+  NODE *next;
 };
 
 struct AdjacencyList {
-  NODE* list[MAXV];
+  NODE *list[MAXV];
   int count = 0;
 };
 
@@ -30,14 +31,15 @@ struct EdgeList {
   int count = 0;
 };
 
-int readFile(const char* fileName, AdjacencyList& list, int flag);
-int countNode(NODE* node);
-int writeFile(const char* fileName, AdjacencyList& list, int flag);
-void deleteAdjacencyList(AdjacencyList& list);
-void printAdjacencyList(const AdjacencyList& list);
+int readFile(const char *fileName, AdjacencyList &list, int flag);
+int countNode(NODE *node);
+int writeFile(const char *fileName, AdjacencyList &list, int flag);
+void deleteAdjacencyList(AdjacencyList &list);
+void printAdjacencyList(const AdjacencyList &list);
 EdgeList takeListOfEdge(AdjacencyList list);
 void printEdge(EDGE edge);
 int calcutaleInDegrees(AdjacencyList list, int vertex);
 void DowLoad(int index);
 void countDowload();
-//Bai 1
+// Bai 1
+AdjacencyList builtAdjacencyList(EdgeList eList, int numOfVer);
